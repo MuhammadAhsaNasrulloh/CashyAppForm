8221be8ff9554d9fe78cc5dc89a72aa7153d1a47
MuhammadAhsaNasrulloh <san.asrullah07@gmail.com>
2023-12-28 20:08:40 +0700

    Add project files.

---
+++ CashyAppForm/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace CashyAppForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.txtBarang = new System.Windows.Forms.TextBox();
+            this.txtKodeBarang = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.txtHarga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnUbah = new System.Windows.Forms.Button();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(273, 46);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(221, 32);
+            this.title.TabIndex = 0;
+            this.title.Text = "MIMAKE MART";
+            // 
+            // txtBarang
+            // 
+            this.txtBarang.Location = new System.Drawing.Point(202, 117);
+            this.txtBarang.Name = "txtBarang";
+            this.txtBarang.Size = new System.Drawing.Size(184, 22);
+            this.txtBarang.TabIndex = 1;
+            // 
+            // txtKodeBarang
+            // 
+            this.txtKodeBarang.Location = new System.Drawing.Point(202, 163);
+            this.txtKodeBarang.Name = "txtKodeBarang";
+            this.txtKodeBarang.Size = new System.Drawing.Size(184, 22);
+            this.txtKodeBarang.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BARANG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "K. BARANG";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(614, 119);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(184, 22);
+            this.txtStock.TabIndex = 5;
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Location = new System.Drawing.Point(614, 165);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.Size = new System.Drawing.Size(184, 22);
+            this.txtHarga.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(492, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "STOCK";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(492, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "HARGA";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(75, 272);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(723, 261);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTambah.Location = new System.Drawing.Point(75, 214);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(113, 36);
+            this.btnTambah.TabIndex = 9;
+            this.btnTambah.Text = "TAMBAH";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHapus.Location = new System.Drawing.Point(212, 214);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(113, 36);
+            this.btnHapus.TabIndex = 10;
+            this.btnHapus.Text = "HAPUS";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnUbah
+            // 
+            this.btnUbah.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUbah.Location = new System.Drawing.Point(349, 214);
+            this.btnUbah.Name = "btnUbah";
+            this.btnUbah.Size = new System.Drawing.Size(113, 36);
+            this.btnUbah.TabIndex = 11;
+            this.btnUbah.Text = "EDIT";
+            this.btnUbah.UseVisualStyleBackColor = true;
+            this.btnUbah.Click += new System.EventHandler(this.btnUbah_Click);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeluar.Location = new System.Drawing.Point(685, 214);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(113, 36);
+            this.btnKeluar.TabIndex = 12;
+            this.btnKeluar.Text = "KELUAR";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 560);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnUbah);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKodeBarang);
+            this.Controls.Add(this.txtBarang);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.TextBox txtBarang;
+        private System.Windows.Forms.TextBox txtKodeBarang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.TextBox txtHarga;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnUbah;
+        private System.Windows.Forms.Button btnKeluar;
+    }
+}
+
---
+++ CashyAppForm/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashyAppForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable table = new DataTable();
+        int indexRow;
+        int selectedRow;
+        int selectedRowIndex;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("Barang", typeof(string));
+            table.Columns.Add("Kode Barang", typeof(string));
+            table.Columns.Add("Stok", typeof(int));
+            table.Columns.Add("Harga", typeof(int));
+
+            dataGridView1.DataSource = table;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            indexRow = e.RowIndex;
+
+            DataGridViewRow row = dataGridView1.Rows[indexRow];
+            txtBarang.Text = row.Cells[0].Value.ToString();
+            txtKodeBarang.Text = row.Cells[1].Value.ToString();
+            txtStock.Text = row.Cells[2].Value.ToString();
+            txtHarga.Text = row.Cells[3].Value.ToString();
+
+            selectedRowIndex = e.RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[indexRow];
+            txtBarang.Text = selectedRow.Cells[0].Value.ToString();
+            txtKodeBarang.Text = selectedRow.Cells[1].Value.ToString();
+            txtStock.Text = selectedRow.Cells[2].Value.ToString();
+            txtHarga.Text = selectedRow.Cells[3].Value.ToString();
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            if (txtBarang.Text == string.Empty || txtKodeBarang.Text == string.Empty || txtStock.Text == string.Empty || txtHarga.Text == string.Empty)
+            {
+                MessageBox.Show("Isi dengan Lengkap");
+            }
+            else
+            {
+                table.Rows.Add(txtBarang.Text, txtKodeBarang.Text, txtStock.Text, txtHarga.Text);
+                dataGridView1.DataSource = table;
+            }
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(rowIndex);
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow newDataRow = dataGridView1.Rows[indexRow];
+            newDataRow.Cells[0].Value = txtBarang.Text;
+            newDataRow.Cells[1].Value = txtKodeBarang.Text;
+            newDataRow.Cells[2].Value = txtStock.Text;
+            newDataRow.Cells[3].Value = txtHarga.Text;
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
